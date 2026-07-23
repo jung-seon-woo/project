98d3aae04fad8ea21e9ba67443bafe72fb50f9f5
jung-seon-woo <alsrn6556@gmail.com>
2026-07-23 12:13:13 +0900

    Change API URL

--- streamlit/app.py
+++ streamlit/app.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 import requests
 from pathlib import Path
-API_URL = os.getenv("API_URL","https://project-hdg3.onrender.com/predict")
+API_URL = os.getenv("API_URL","https://project-car-g83q.onrender.com/predict")
 
 #페이지설정
 st.set_page_config(
